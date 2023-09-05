4e8392d7b12f1c15293d6400ffb4fbaf86b619ae
ijiejerry <143167220+ijiejerry@users.noreply.github.com>
2023-09-05 14:10:21 +0300

    Initial commit

---
+++ src/MyApp.java
@@ -0,0 +1,36 @@
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Printer App");
+		
+		System.out.println("This app is made by");
+		
+		String creator = "Jerry";
+		System.out.println("This app is made by" + " " + creator);
+		
+		int num1 = 7;
+		int num2 = 10;
+		System.out.println(num1);
+		System.out.println("The value of num1 is 4");
+		System.out.println("The value of num1 is 7");
+		System.out.println("The value of num2 is 10");
+		
+//	Declear integer typed variable called multiplication;
+		
+		System.out.println(num1 * num2);
+		System.out.println("7 * 10 = 70");
+		System.out.println("4 * 10 = 40");
+		
+//		integer typed variable called add,div & subt
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 - num2);
+		
+	}
+	
+
+}
